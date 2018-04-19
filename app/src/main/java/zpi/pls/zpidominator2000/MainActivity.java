a37661f11dfd79d954a5912637c4a87cd7d6d5a0
shorti1996 <shorti1996@gmail.com>
2018-04-19 13:51:45 +0200

    Initial commit

---
+++ app/src/main/java/zpi/pls/zpidominator2000/MainActivity.java
@@ -0,0 +1,155 @@
+package zpi.pls.zpidominator2000;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Witaj w domku", Snackbar.LENGTH_SHORT)
+//                        .setAction("Action", this)
+                        .show();
+                goToHomePlan();
+            }
+        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        goToHomePlan();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.nav_rooms) {
+//            goToRooms();
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToHomePlan() {
+        // Create new fragment and transaction
+        Fragment newFragment = new HomePlanFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.replace(R.id.fragmentsContainer, newFragment);
+//        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+
+        getDrawerView().setCheckedItem(R.id.nav_home);
+    }
+
+    private void goToRooms() {
+        // Create new fragment and transaction
+        Fragment newFragment = new RoomsFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.replace(R.id.fragmentsContainer, newFragment);
+//        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+
+        getDrawerView().setCheckedItem(R.id.nav_rooms);
+    }
+
+    private NavigationView getDrawerView() {
+        if (drawerView == null) {
+            drawerView = drawerLayout.findViewById(R.id.nav_view);
+            return (NavigationView) drawerView;
+        } else {
+            return (NavigationView) drawerView;
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_rooms) {
+            goToRooms();
+            closeDrawers();
+            return true;
+        } else if (id == R.id.nav_home) {
+            goToHomePlan();
+            closeDrawers();
+            return true;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void closeDrawers() {
+        drawerLayout.closeDrawers();
+    }
+}
