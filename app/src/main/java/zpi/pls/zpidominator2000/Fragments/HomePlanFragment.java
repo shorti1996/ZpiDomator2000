9191c4b5ac87a766a7cc2aada5c460af80c64ae7
shorti1996 <shorti1996@gmail.com>
2018-05-26 15:21:49 +0200

    clicking on particular rooms works

--- app/src/main/java/zpi/pls/zpidominator2000/Fragments/HomePlanFragment.java
+++ app/src/main/java/zpi/pls/zpidominator2000/Fragments/HomePlanFragment.java
@@ -3,6 +3,7 @@ package zpi.pls.zpidominator2000.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import zpi.pls.zpidominator2000.R;
+import zpi.pls.zpidominator2000.Utils;
 
 
 /**
@@ -76,25 +78,24 @@ public class HomePlanFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_plan, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        homePlanIv = getView().findViewById(R.id.homeFromAboveIv);
-        rootView = getView().findViewById(R.id.ConstraintLayoutRoot);
-        floorText = getView().findViewById(R.id.floorTv);
-        homeName = getView().findViewById(R.id.home_name);
+        rootView = view.findViewById(R.id.ConstraintLayoutRoot);
+        floorText = view.findViewById(R.id.floorTv);
+        homeName = view.findViewById(R.id.home_name);
 /*        homePlanIv.setOnClickListener(view1 -> {
             swapFloor();
         });*/
-
         loadFloor(isOnZeroFloor);
+
     }
 
     public void swapFloor() {
@@ -103,18 +104,60 @@ public class HomePlanFragment extends Fragment {
     }
 
     private void loadFloor(boolean isOnGroundFloor) {
-        if (isOnGroundFloor) {
-            Glide.with(this)
-                    .load(R.drawable.parter)
-                    .into(homePlanIv);
-//            floorText.setText(R.string.ground_floor);
-            homeName.setText(R.string.ground_floor);
-        } else {
-            Glide.with(this)
-                    .load(R.drawable.pietro1)
-                    .into(homePlanIv);
-//            floorText.setText(R.string.first_floor);
-            homeName.setText(R.string.first_floor);
+        View view = getView();
+        if (view != null) {
+            ViewGroup floor_content = view.findViewById(R.id.include_floor);
+            floor_content.removeAllViews();
+            if (isOnGroundFloor) {
+                getLayoutInflater().inflate(R.layout.parter_layout, floor_content);
+                homePlanIv = view.findViewById(R.id.homeFromAboveIv);
+                Glide.with(this)
+                        .load(R.drawable.parter)
+                        .into(homePlanIv);
+                homeName.setText(R.string.ground_floor);
+
+                view.findViewById(R.id.button_parter1_1).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "1");
+                });
+                view.findViewById(R.id.button_parter1_2).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "1");
+                });
+                view.findViewById(R.id.button_parter2).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "2");
+                });
+                view.findViewById(R.id.button_parter3).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "3");
+                });
+                view.findViewById(R.id.button_parter4).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "4");
+                });
+                view.findViewById(R.id.button_parter5).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "5");
+                });
+            } else {
+                getLayoutInflater().inflate(R.layout.pietro1_layout, floor_content);
+                homePlanIv = view.findViewById(R.id.homeFromAboveIv);
+                Glide.with(this)
+                        .load(R.drawable.pietro1)
+                        .into(homePlanIv);
+                homeName.setText(R.string.first_floor);
+
+                view.findViewById(R.id.button_pietro1_1).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "1");
+                });
+                view.findViewById(R.id.button_pietro1_2).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "2");
+                });
+                view.findViewById(R.id.button_pietro1_3).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "3");
+                });
+                view.findViewById(R.id.button_pietro1_4).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "4");
+                });
+                view.findViewById(R.id.button_pietro1_5).setOnClickListener(v -> {
+                    Utils.showToast(getContext(), "5");
+                });
+            }
         }
     }
 
